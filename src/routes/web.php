74c4601923aae42b5b1d4cd77b37a70156baa04a
JPedroCavalcante <jpc.almeida2@gmail.com>
2023-10-21 19:18:02 -0300

    [DLT] Removendo Dashboard

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/jobs', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
